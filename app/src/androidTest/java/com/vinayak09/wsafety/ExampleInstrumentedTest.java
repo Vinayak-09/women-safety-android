4d58972649c170368e1a667c6770965dc3dbf4db
MK <72601407+Miten94@users.noreply.github.com>
2024-02-24 16:42:25 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/vinayak09/wsafety/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/vinayak09/wsafety/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.vinayak09.wsafety;
+package com.Miten94.wsafety;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.vinayak09.wsafety", appContext.getPackageName());
+        assertEquals("com.Miten.wsafety", appContext.getPackageName());
     }
 }
